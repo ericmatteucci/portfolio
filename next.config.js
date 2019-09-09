356efb6e0e5b9f1d7db706acd1ea14fd9cd482a4
Eric Matteucci <eric.matteucci@ucalgary.ca>
2019-09-09 13:37:52 -0700

    Added sass stylings and loader and skeleton for a header component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const withSASS = require('@zeit/next-sass');
+
+module.exports = withSASS({
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
@@ -7,4 +9,4 @@ module.exports = {
 
     return config
   }
-};
+});
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.scss';
+
+type HeaderPropTypes = {};
+
+class Header extends React.Component<HeaderPropTypes> {
+
+    render() {
+        return (
+            <div className="main">HELLO WORLD</div>
+        );
+    }
+};
+
+export default Header;
